cb76c35012d359373e3a1b8f9d1dd81addf623e3
Danielius Kramnik <dkramnik@google.com>
2024-12-28 14:00:54 -0800

    parse GDS to pad list

--- gdsfootprint/gdsfootprint.py
+++ gdsfootprint/gdsfootprint.py
@@ -1,10 +1,154 @@
-# Todo
+# Imports
+from dataclasses import dataclass # https://stackoverflow.com/questions/35988/c-like-structures-in-python
+
+# KLayout Python API
+import pya
+
+# Define data class representing a pad (extract from GDS, then send to output writer)
+@dataclass
+class Pad:
+	x_um : float
+	y_um : float
+	x_size_um : float
+	y_size_um : float
+	name : str
+
+# ================================================================================================
+# Main methods
+# ================================================================================================
 
 def parse_gds( gds_file_in, label_layer_in, search_by_cell_name, search_cell_name = None, search_layer = None ):
-	return False
+	# =================================================================
+	print( 'Loading input gds file ', self.gds_file_in )
+	# =================================================================
+
+	layout_in = pya.Layout( )
+	layout_in.read( self.gds_file_in )
+	topcell_in = layout_in.top_cell( )
+
+	print( 'Layout dbu =', layout_in.dbu )
+
+	# =================================================================
+	print( 'Searching for pads...' )
+	# =================================================================
+
+	cupillar_matches = search_for_insts_with_trans_by_name( topcell_in, 'curcvpad' )
+	cupillar_trans_list = [ x[ 1 ] for x in cupillar_matches ]
+	cupillar_coord_list = [ ( trans.disp.x * layout_in.dbu, trans.disp.y * layout_in.dbu ) for trans in cupillar_trans_list ]
+
+	# Remove any duplicates (next time check for duplicates in the source GDS within the footprint generation script and prune them there)
+	cupillar_coord_list = set( cupillar_coord_list )
+
+	print( 'Found {:d} curcvpads in GDS footprint'.format( len( cupillar_coord_list ) ) )
+
+	# =================================================================
+	print( 'Searching for labels...' )
+	# =================================================================
+
+	# Get the layer index that corresponds to the lpp we want to search for in the input layout
+	search_layer = layout_in.find_layer( self.label_layer_info_in )
+	label_trans_tuple_list = find_text_labels_and_trans( search_layer, topcell_in )
+	label_coord_list = [ ( trans.disp.x * layout_in.dbu, trans.disp.y * layout_in.dbu ) for ( label, trans ) in label_trans_tuple_list ]
+	label_list = [ label for ( label, trans ) in label_trans_tuple_list ]
+
+	print( 'Found {:d} labels in GDS footprint'.format( len( label_trans_tuple_list ) ) )
+
+	# Check that the number of pads and labels matches
+	assert( len( cupillar_coord_list ) == len( label_trans_tuple_list ) )
+
+	# =================================================================
+	print( 'Associating pads with labels and building pad list...' )
+	# =================================================================
+
+	pad_list = [ ]
+	label_index_list = [ ]
+
+	for pad_coord in cupillar_coord_list:
+		dist_list = [ ]
+
+		# Iterate through labels and compute the distance to each one,
+		# go through whole list each time so that any labels that could be associated with multiple pads are caught
+		for label_coord in label_coord_list:
+			pad_to_label_dist = np.sqrt( ( pad_coord[ 0 ] - label_coord[ 0 ] )**2 + ( pad_coord[ 1 ] - label_coord[ 1 ] )**2 )
+			dist_list.append( pad_to_label_dist )
+
+		# Find the label with minimum distance
+		label_index = dist_list.index( min( dist_list ) )			
+		label_index_list.append( label_index )
+
+		# Add pad entry
+		pad_list.append(
+			Pad(
+				x_um = pad_coord[ 0 ],
+				y_um = pad_coord[ 1 ],
+				x_size_um = 105,
+				y_size_um = 105,
+				name = label_list[ label_index ].translate( { ord( '<' ) : '_', ord( '>' ) : None } ) # Replace Pin<X> with Pin_X
+			)
+		)
+
+	# Check that the label index associations were unique (no single label associated with multiple pads)
+	assert( len( label_index_list ) == len( label_list ) )
+
+	# =================================================================
+	print( 'Done parsing!\n' )
+	# =================================================================
+	return pad_list
 
 def generate_footprint_gds( parsed_gds_in, gds_file_interposer_pad, label_layer_out, gds_file_out ):
 	return False
 
 def generate_footprint_altium_scripts( parsed_gds_in, altium_sym_script_out, altium_fp_script_out ):
 	return False
+
+# ================================================================================================
+# Utility methods
+# ================================================================================================
+
+# Utility method for searching for pads in GDS
+def search_for_insts_with_trans_by_name( cell, name_str, trans = pya.Trans.new( 0, 0 ) ):
+	inst_list = cell.each_inst( )
+	matches = [ ]
+
+	for inst in inst_list:
+		if inst.is_regular_array( ):
+			#print( 'Scanning array {:d} X {:d} array:'.format( inst.na, inst.nb ), inst.cell.name )
+			for i in range( inst.na ):
+				for j in range( inst.nb ):				
+					trans_i = pya.Trans( int( i * inst.da.x / inst.cell.layout( ).dbu ), int( i * inst.da.y / inst.cell.layout( ).dbu ) )
+					trans_j = pya.Trans( int( j * inst.db.x / inst.cell.layout( ).dbu ), int( j * inst.db.y / inst.cell.layout( ).dbu ) )
+					array_item_trans = trans * inst.trans * trans_j * trans_i
+					if name_str in inst.cell.name:
+						#print( 'Detected matching inst that is a {:d} X {:d} array:'.format( inst.na, inst.nb ), inst.cell.name )
+						#print( array_item_trans )
+						matches.append( [ inst, array_item_trans ] )
+					else:
+						new_matches = search_for_insts_with_trans_by_name( inst.cell, name_str, array_item_trans )
+						if len( new_matches ) > 0 :
+							matches += new_matches
+		else:
+			if name_str in inst.cell.name:
+				matches.append( [ inst, trans * inst.trans ] )
+			else:
+				new_matches = search_for_insts_with_trans_by_name( inst.cell, name_str, trans * inst.trans )
+				if len( new_matches ) > 0 :
+					matches += new_matches
+
+	return matches
+
+# Utility method for searching for labels in GDS
+def find_text_labels_and_trans( search_layer, cell, trans = pya.Trans.new( 0, 0 ) ):
+	matches = [ ]
+
+	# Iterate through all shapes (texts) in the cell
+	for shape in cell.each_shape( search_layer ):
+		if shape.is_text( ):
+			matches.append( ( shape.text.string, trans * shape.text_trans ) )
+
+	# Recursively search through instances (child cells)
+	for inst in cell.each_inst( ):
+		new_matches = find_text_labels_and_trans( search_layer, inst.cell, trans * inst.trans )
+		if len( new_matches ) > 0 :
+			matches += new_matches
+
+	return matches
